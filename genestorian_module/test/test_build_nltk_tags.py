1faf844232db3acbc7cb49fa56c074564eb29e8e
anamika-yadav99 <54634163+anamika-yadav99@users.noreply.github.com>
2022-09-06 19:16:52 +0530

    Update test_build_nltk_tags.py

--- genestorian_module/test/test_build_nltk_tags.py
+++ genestorian_module/test/test_build_nltk_tags.py
@@ -32,7 +32,6 @@ class TestBuildNltkTags(unittest.TestCase):
             '../../data/alleles.toml',
             '../../data/gene_IDs.toml',
             '../../allele_components/tags.toml',
-            '../../allele_components/tags_fpbase.toml',
             '../../allele_components/markers.toml',
             '../../allele_components/promoters.toml'
         ]
